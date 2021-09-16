bee3f63830ae655d68d742f6c6bb26b98ba64b46
rafae <rafae@Rafael-GS>
2021-09-16 13:30:07 -0300

    Update

--- src/controller/NPRController.java
+++ src/controller/NPRController.java
@@ -18,45 +18,55 @@ public class NPRController {
 	public int npr(Pilha p, String op) throws Exception {
 		char c ;
 		int arg1,arg2,res;
+		
+		if(op.length() < 3) {
+			System.err.println("Não há elementos suficientes para fazer a operação!");
+		}
+		else {
         for (int i=0; i<op.length(); i++) {
              c = op.charAt(i);
             if (Character.isDigit(c))
-                p.push(Character.digit(c,10));
+                insereValor(p,Character.digit(c,10));
             else if(c=='+') {
                 arg1 = p.top(); p.pop();
                 arg2 = p.top(); p.pop();
-                p.push(arg1+arg2);
+                insereValor(p,arg1+arg2);
             }
             else if(c=='*') {
                 arg1 = p.top(); p.pop();
                 arg2 = p.top(); p.pop();
-                p.push(arg1*arg2);
+                insereValor(p,arg1*arg2);
             }
             else if(c=='-') {
                 arg1 = p.top(); p.pop();
                 arg2 = p.top(); p.pop();
                 if(arg1 > arg2) {
-                    p.push(arg1-arg2);
-                    }else {
-                    	p.push(arg2-arg1);
-                    }
+                	insereValor(p,arg1-arg2);
+                  }
+                else {
+                   	insereValor(p,arg2-arg1);
+                  }
             }
             else if(c=='/') {
                 arg1 = p.top(); p.pop();
                 arg2 = p.top(); p.pop();
                 if(arg1 > arg2) {
-                p.push(arg1/arg2);
-                }else {
-                	p.push(arg2/arg1);
+                	insereValor(p,arg1/arg2);
+                }
+                else {
+                	insereValor(p,arg2/arg1);
                 }
             }
             else if(c=='^') {
                 arg1 = p.top(); p.pop();
                 arg2 = p.top(); p.pop();
-                p.push((int) Math.pow(arg1,arg2));
+                insereValor(p,(int) Math.pow(arg1,arg2));
             }
         }
-        res = p.top();p.pop();
+		}
+        res = p.top();
+        res = p.pop();
+        insereValor(p,res);
         return res;
 	}
 }
